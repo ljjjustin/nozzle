56d94005b4ebda159e5729311987197250b9c7bb
Jiajun Liu <iamljj@gmail.com>
2012-10-15 17:46:46 +0800

    add endpoint related exception

--- nozzle/common/exception.py
+++ nozzle/common/exception.py
@@ -49,6 +49,13 @@ class Forbidden(NozzleException):
     message = _("Forbidden: your credentials don't give you "
                 "access to this resource")
 
+class EndpointNotFound(NozzleException):
+    message = _("endpoint not found")
+
+
+class AmbiguousEndpoints(NozzleException):
+    message = _("ambigous endpoint were found")
+
 
 # api exceptions
 class MissingParameter(NozzleException):
